a1b374a4d8eb0436d657972bf5bc6ce7e7cdd055
cesardavid26 <cesardavidcp@ufps.edu.co>
2020-11-21 23:11:16 -0500

    se añadio la clase prueba

--- src/util/Prueba.java
+++ src/util/Prueba.java
@@ -1,10 +1,37 @@
 package util;
 
+import dao.EstadoDao;
+import dao.GeneroDao;
+import dao.HeroeDao;
+import modelo.Estado;
+import modelo.Genero;
+import modelo.Heroe;
+
 public class Prueba {
 
 	public static void main(String[] args) {
 	
 		
+		
+		Genero g = new Genero();
+		g.setDescripcion("XDD");
+		g.setId("X");
+		GeneroDao gdao = new GeneroDao();
+		gdao.insert(g);
+		
+		Estado e = new Estado();
+		e.setDescripcion("LOOL");
+		e.setId("L");
+		EstadoDao edao = new EstadoDao();
+		edao.insert(e);
+		
+		Heroe h = new Heroe();
+		h.setNombre("prueba");
+		h.setHeroe("Super prueba");
+		h.setEstadoBean(e);
+		h.setGeneroBean(g);
+		HeroeDao cdao = new HeroeDao();
+		cdao.insert(h);
 
 	}
 
